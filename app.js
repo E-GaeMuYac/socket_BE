4487f0b6fcb2ac9eef7697ee779512c150d01408
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-24 15:36:19 +0900

    FEAT:CICD 적용

--- app.js
+++ app.js
@@ -13,5 +13,9 @@ io.on("connection", (socket) => {
   });
 });
 
+app.get('/',(req, res) => {
+ res.send('CICD')
+})
+
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 http.listen(3000, handleListen);
---
+++ scripts/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+cd /home/ubuntu/socket_BE
+authbind --deep pm2 start app.js
