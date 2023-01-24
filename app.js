ef1a0a28bacb570046dafb46c732ca47fa27d611
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-24 23:17:53 +0900

    FEAT:nickname 추가 및 disconnecting 추가

--- app.js
+++ app.js
@@ -8,14 +8,24 @@ app.use(cors());
 
 io.on("connection", (socket) => {
   socket.on("chatting", (data) => {
-    console.log(data)
-    io.emit("chatting", data)
+    socket.join(data);
+
+    socket.to(data).emit(`${socket.nickname}가 입장했습니다.`);
   });
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach((room) =>
+      socket.to(room).emit(`${socket.nickname}가 떠났습니다.`)
+    );
+  });
+  socket.on("new_message", (msg, roomName) => {
+    socket.to(roomName).emit("new_message", `${socket.nickname} : ${msg}`);
+  });
+  socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
-app.get('/',(req, res) => {
- res.send('pillnuts Chatting Server')
-})
+app.get("/", (req, res) => {
+  res.send("pillnuts Chatting Server");
+});
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 http.listen(3000, handleListen);
