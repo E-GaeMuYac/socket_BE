87f648988a3e8520c5adf76f622330a82bf69388
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-25 00:28:50 +0900

    fix:CORS에러

--- app.js
+++ app.js
@@ -1,20 +1,20 @@
 const express = require("express");
 const cors = require("cors");
+app.use(cors());
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 require("dotenv").config();
-app.use(cors());
+
 
 io.on("connection", (socket) => {
   socket.on("chatting", (data) => {
     socket.join(data);
-
-    socket.to(data).emit(`${socket.nickname}가 입장했습니다.`);
+    socket.to(data).emit(`${socket.nickname}가 입장했습니다. ID : ${socket.id}`);
   });
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
-      socket.to(room).emit(`${socket.nickname}가 떠났습니다.`)
+      socket.to(room).emit(`${socket.nickname}가 떠났습니다. ID : ${socket.id}`)
     );
   });
   socket.on("new_message", (msg, roomName) => {
@@ -24,7 +24,7 @@ io.on("connection", (socket) => {
 });
 
 app.get("/", (req, res) => {
-  res.send("pillnuts Chatting Server");
+  res.send("pillnuts Chatting Server!");
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
