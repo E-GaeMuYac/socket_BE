cef21345590e276bb78e9668b6cc78d8db0b5224
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-24 14:51:13 +0900

    feat:소켓서버

---
+++ app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const http = require("http").createServer(app);
+const io = require("socket.io")(http);
+require("dotenv").config();
+app.use(cors());
+
+io.on("connection", (socket) => {
+  socket.on("chatting", (data) => {
+    console.log(data)
+    io.emit("chatting", data)
+  });
+});
+
+const handleListen = () => console.log(`Listening on http://localhost:3000`);
+http.listen(3000, handleListen);
