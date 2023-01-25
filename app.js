7d391ca06bde1d37b5c0c55b1f82b17aabc5a21e
kimmand0o0 <yuwang0130@gmail.com>
2023-01-25 17:50:13 +0900

    :sparkles: #1 FEAT : 로거 기능 구현

--- app.js
+++ app.js
@@ -1,21 +1,54 @@
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const http = require("http").createServer(app);
-const io = require("socket.io")(http);
-require("dotenv").config();
 app.use(cors());
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+require("dotenv").config();
+
+const logger = require("./logger/logger");
 
 io.on("connection", (socket) => {
+  logger.info("socketId : ", { message: socket.id });
   socket.on("chatting", (data) => {
-    console.log(data)
-    io.emit("chatting", data)
+    logger.info("data : ", { message: data });
+    io.emit("chatting", data);
+    socket.join(data);
+    socket
+      .to(data)
+      .emit("join", `${socket.nickname}가 입장했습니다. ID : ${socket.id}`);
   });
+
+  socket.on("disconnecting", () => {
+    socket.rooms.forEach(
+      (room) =>
+      socket
+        .to(room)
+        .emit("leave", `${socket.nickname}가 떠났습니다. ID : ${socket.id}`)
+    );
+    logger.info("leaveRoom : ", {
+      message: `${socket.nickname}가 떠났습니다. ID : ${socket.id}`,
+    })
+  });
+
+  socket.on("new_message", (msg, roomName) => {
+    logger.info("new_message : ", { message: `roomName : ${roomName}, msg : ${msg}` });
+    socket.to(roomName).emit("new_message", `${socket.nickname} : ${msg}`);
+  });
+
+  socket.on("nickname", (nickname) => {
+    logger.info("nickname : ", { message: nickname });
+    (socket["nickname"] = nickname)});
 });
 
-app.get('/',(req, res) => {
- res.send('CICD6')
-})
+app.get("/", (req, res) => {
+  res.send("pillNuts Chatting Server!!");
+});
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
 http.listen(3000, handleListen);
