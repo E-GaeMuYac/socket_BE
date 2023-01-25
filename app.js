218ad0c165152399ab7d9306faefaa6d59f951c1
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-25 14:18:52 +0900

    fix: 이벤트 추가

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const app = express();
 app.use(cors());
 const http = require("http").createServer(app);
-const io = require('socket.io')(http, {
+const io = require("socket.io")(http, {
   cors: {
     origin: "*",
     methods: ["GET", "POST"],
@@ -11,25 +11,29 @@ const io = require('socket.io')(http, {
 });
 require("dotenv").config();
 
-
 io.on("connection", (socket) => {
   socket.on("chatting", (data) => {
     socket.join(data);
-    socket.to(data).emit(`${socket.nickname}가 입장했습니다. ID : ${socket.id}`);
+    socket
+      .to(data)
+      .emit("join", `${socket.nickname}가 입장했습니다. ID : ${socket.id}`);
   });
+
   socket.on("disconnecting", () => {
     socket.rooms.forEach((room) =>
-      socket.to(room).emit(`${socket.nickname}가 떠났습니다. ID : ${socket.id}`)
+      socket.to(room).emit("leave",`${socket.nickname}가 떠났습니다. ID : ${socket.id}`)
     );
   });
+
   socket.on("new_message", (msg, roomName) => {
     socket.to(roomName).emit("new_message", `${socket.nickname} : ${msg}`);
   });
+
   socket.on("nickname", (nickname) => (socket["nickname"] = nickname));
 });
 
 app.get("/", (req, res) => {
-  res.send("pillnuts Chatting Server!");
+  res.send("pillNuts Chatting Server!!");
 });
 
 const handleListen = () => console.log(`Listening on http://localhost:3000`);
