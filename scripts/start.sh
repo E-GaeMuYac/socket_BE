ef425a43f29e48cd9e34042f4a140402e273b6fb
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-01-28 16:51:03 +0900

    feat : 폴더구조 개선

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-cd /home/ubuntu/socket_BE
+cd /home/ubuntu/socket_BE/src
 authbind --deep pm2 start server.js
--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /home/ubuntu/socket_BE
+cd /home/ubuntu/socket_BE/src
 pm2 stop server.js 2> /dev/null || true
 pm2 delete server.js 2> /dev/null || true
