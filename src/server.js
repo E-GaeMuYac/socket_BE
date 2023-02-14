35e6577f1402306146130b77ad88afea3c330ad9
youngjae0411 <103705842+youngjae0411@users.noreply.github.com>
2023-02-14 22:23:52 +0900

    fix : 서버문제

--- src/server.js
+++ src/server.js
@@ -1,7 +1,8 @@
 const http = require('./app');
+const logger = require('../logger/logger');
 
 require('./socket');
 
 http.listen(3000, () => {
-  console.log(`Listening on http://localhost:3000`);
+  logger.log(`Listening on http://localhost:3000`);
 });
